0a9d2de4a75803e0eb0fa788cedb9d5955091d6c
Daniil Siamenka <maloigb@gmail.com>
2023-10-19 23:29:57 +0300

    add hook use-auth

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,13 @@
+import {useAppSelector} from "./redux.ts";
+
+
+export const useAuth = () => {
+    const {email, token, id} = useAppSelector(state => state.user);
+
+    return {
+        isAuth: !!email,
+        email,
+        token,
+        id,
+    }
+};
