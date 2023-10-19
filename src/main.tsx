e142f49461df3c1178baad61f04afc35e13cef67
Daniil Siamenka <maloigb@gmail.com>
2023-10-20 00:06:19 +0300

    configured firebase

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import {store} from "./store";
+import './firebase.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
